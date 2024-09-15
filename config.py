59c2e902eafa1dab625ef98b9306c8b1908d9050
Georgy <160395796+Georgyrs@users.noreply.github.com>
2024-09-15 16:53:42 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,18 @@
+BOT_TOKEN = '7392365900:AAHxzmmwXTzNOGT4jVnNS6zEKAB_NBCpgWY'
+DATABASE_NAME = 'bot_database.db'
+
+
+WEATHER_URL = "https://rp5.ru/Погода_в_Калининграде"
+WEATHER_API_URL = "https://rusmeteo.net/weather/kaliningrad/14days/"
+
+
+WIKIPEDIA_LANGUAGE = 'ru'
+
+
+MEDIA_PATHS = {
+    1: r'C:\Users\Asus\PycharmProjects\pythonProject6\static\photo_2024-08-23_16-25-17.jpg',
+    2: r'C:\Users\Asus\PycharmProjects\pythonProject6\static\IMG_2038.MP4',
+    3: r'C:\Users\Asus\PycharmProjects\pythonProject6\static\IMG_2041.MP4',
+    4: r'C:\Users\Asus\PycharmProjects\pythonProject6\static\photo_2024-08-23_16-25-24.jpg',
+    5: r'C:\Users\Asus\PycharmProjects\pythonProject6\static\photo_2024-08-23_16-25-27.jpg'
+}
